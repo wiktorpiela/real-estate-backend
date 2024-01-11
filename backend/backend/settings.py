0702a353949285ae296da2ae0c88bafb021f913b
goesoftwarePY <wpiela9@gmail.com>
2024-01-11 07:41:02 +0100

    user register - started

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -146,3 +146,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
 }
+
+DJOSER = {
+    'USER_CREATE_PASSWORD_RETYPE' : True,
+    'SEND_ACTIVATION_EMAIL': False,
+}
