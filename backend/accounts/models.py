a5a78aaa8b9ec4c8c60c168bc46d6f2740a77ca1
Wiktor Piela <wpiela9@gmail.com>
2024-01-24 06:54:09 +0100

    user profile added (seller) and agency_name field applied to listings model serializer

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# class Profile(models.Model):
+class Profile(models.Model):
+    seller = models.OneToOneField(User, on_delete=models.CASCADE)
+    agency_name = models.CharField(max_length=100, blank=True, null=True)
+    phone_number = models.CharField(max_length=25, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    profile_pic = models.ImageField(upload_to = 'profile_pictures/%Y/%m/%d/', blank=True, null=True)
 
+    def __str__(self) -> str:
+        return f'Profile of {self.seller.username}'
--- backend/listings/serializers.py
+++ backend/listings/serializers.py
@@ -3,6 +3,10 @@ from .models import Listing
 
 class ListingSerializer(serializers.ModelSerializer):
     seller = serializers.StringRelatedField()
+    seller_agency_name = serializers.SerializerMethodField()
+
+    def get_seller_agency_name(self, obj):
+        return obj.seller.profile.agency_name
     
     class Meta:
         model = Listing
